52f7b4ac2ef74b01c8f279f5b93c00c860fe9d76
MichalDolata <dev@dolata.me>
2016-12-11 18:24:46 +0100

    Repair folder structure

---
+++ day6/script.js
@@ -0,0 +1,6 @@
+/**
+ * Created by preb on 11.12.16.
+ */
+const data = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities
